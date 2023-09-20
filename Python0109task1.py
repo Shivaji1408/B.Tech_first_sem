5132f5134f8a65a5064547e6e4c612921590d6a3
Shivaji1408 <142593011+Shivaji1408@users.noreply.github.com>
2023-09-20 11:51:36 +0530

    Add files via upload

---
+++ Python0109task1.py
@@ -0,0 +1,11 @@
+#  Problem : Write a program that takes two numbers as input from the user and performsbasic arthmatic operation
+# (Addition, Substraction, Multiplication, Devision) on them
+print("Input the value of num1 = ")
+num1 = int(input())
+print("Input the value of num2 = ")
+num2 = int(input())
+print(f"the addition of num1 and num2 = {num1 + num2}")
+print(f"the substraction of num1 and num2 = {num1 - num2}")
+print(f"the multiplicatin of num1 and num2 = {num1*num2}")
+print(f"the devision of num1 and num2 = {num1//num2}")
+
